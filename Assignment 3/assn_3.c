c0592c51def8949b27b7f54247a15ee17a6bec97
Ritesh Narendra Ghorse <rghorse@ncsu.edu>
2020-04-01 19:38:15 -0400

    multistep done

--- Assignment 3/assn_3.c
+++ Assignment 3/assn_3.c
@@ -11,139 +11,24 @@ typedef struct file_record{
     int flag;   //if all files are read
 }file_record;
 
-int file_count = 0, total_runs = 0;
+int file_count = 0, total_runs = 0, files_read = 0;
 
 char METHODS[3][20] = {"--basic", "--multistep", "--replacement"};
 char method[20], ip_filename[20], op_filename[20];
+
+
+void printBuffer(int buffer[1000])
+{   
+    int i;
+    for(i=0; i<1000; ++i)
+        printf(" %d\n", buffer[i]);
+}
 
 int comparator(const void *first, const void *second)
 {
     int l = *(int*)first;
     int r = *(int*)second;
     return (l - r);
-}
-
-int fillBuffer(int i, int n, int *ip_buffer, file_record *fr, int file_num)
-{
-    FILE *fp;
-    int j, ind=0;
-    char file[30];
-    int buff[1];
-    
-    strcpy(file, fr[i].filename);
-    
-    fp = fopen(file, "rb");
-    // printf("%s", fr[i].filename);
-    fseek(fp, sizeof(int)*fr[i].read_till, SEEK_SET);
-    // printf("indexx\n");
-    if(feof(fp) && file_count < file_num)
-    {
-        fclose(fp);
-        sprintf(file, "input.bin.%3d", file_count);
-        ++file_count;
-        strcpy(fr[i].filename, file);
-        fr[i].flag = 0;
-        fp = fopen(file, "rb");
-    }
-    else if(feof(fp) && file_count >= file_num)
-    {
-        fr[i].flag = 1;
-        fclose(fp);
-        return -1;
-    }
-    fr[i].index = n * i;
-    ind = fr[i].index;
-
-    for(j=0; j<n; ++j)
-    {
-        if(feof(fp))
-        {
-            fr[i].flag = 1;
-            break;
-        }
-        fread(buff, sizeof(int), 1, fp);
-        ip_buffer[ind] = buff[0];
-        ++ind;
-        fr[i].read_till += 1;
-    }
-    fclose(fp);
-    return 0;
-}
-void printBuffer(int buffer[1000])
-{
-    int i;
-    for(i=0; i<1000; ++i)
-        printf(" %d\n", buffer[i]);
-}
-
-void basicMergesort()
-{
-    // printf("in basic\n");
-
-    // we will do 25-way merge
-    file_record fr[25]; 
-    char file[30];
-    int ip_buffer[1000], op_buffer[1000];
-    int i, j, written=0;   
-    int op_index=0, temp_index, min_value;
-    FILE *op;
-    // load 40 keys from each run file
-    file_count = 0;
-    for(i=0; i<total_runs; ++i)
-    {
-        sprintf(file, "input.bin.%03d", file_count);
-        ++file_count;
-        fr[i].read_till = 0;
-        fr[i].flag = 0;
-        strcpy(fr[i].filename, file);
-        fillBuffer(i, 40, ip_buffer, fr, total_runs);
-    }
-    
-    temp_index = 0;
-    while(written < total_runs)
-    {
-        temp_index = -1;
-        min_value = INT_MAX;
-
-        // get the min value assuming first to be the smallest
-        // check if file were empty as well
-        for(i=0; i<total_runs; ++i)
-        {
-            if(fr[i].flag==0 && min_value > ip_buffer[fr[i].index])
-            {
-                temp_index = i;
-                min_value = ip_buffer[fr[i].index];
-            }
-        }
-        // make sure that we found min value
-        if(min_value != INT_MAX)
-        {
-            op_buffer[op_index++] = min_value;
-            ip_buffer[fr[temp_index].index] = INT_MAX;
-            fr[temp_index].index += 1;
-        }
-        // write to file if output buffer is full
-        if(op_index >= 1000)
-        {
-            op = fopen(op_filename, "ab+");
-            fwrite(op_buffer, sizeof(int), 1000, op);
-            fclose(op);
-            ++written;
-            op_index = 0;
-        }
-        // check if the buffer in which min_value was found
-        // is full
-        if(fr[temp_index].index%40 == 0)
-        {
-            fillBuffer(temp_index, 40, ip_buffer, fr, total_runs);
-        }
-    }
-    if(op_index != 0)
-    {
-        op = fopen(op_filename, "ab+");
-        fwrite(op_buffer, sizeof(int), (size_t)op_index, op);
-        fclose(op);
-    }
 }
 
 void createRuns(char filename[20])
@@ -169,69 +54,129 @@ void createRuns(char filename[20])
     }
 }
 
-void merge(int runs, int file_num, int start, char run_file[30],char filename[30])
+int fillBuffer(int i, int n, int *ip_buffer, file_record *fr, char format[30], int file_num)
 {
-    file_record fr[runs];
-    FILE *op;
-    int op_buffer[1000], ip_buffer[1000];
-    int i, written_runs, op_index, temp_index, min_value;
-    int r, size, num;
+    FILE *fp;
+    int j, ind=0;
     char file[30];
-    num = start+runs;
-    size = 1000/runs;
-    // initial setup
+    int buff[1];
+    
+    strcpy(file, fr[i].filename);
+    
+    fp = fopen(file, "rb");
+    // printf("%s", fr[i].filename);
+    fseek(fp, sizeof(int)*fr[i].read_till, SEEK_SET);
+    // printf("indexx\n");
+    if(feof(fp) && file_count < file_num)
+    {
+        fclose(fp);
+        sprintf(file, "%s.%3d", format, file_count);
+        ++file_count;
+        strcpy(fr[i].filename, file);
+        fr[i].flag = 0;
+        fr[i].read_till = 0;
+        fp = fopen(file, "rb");
+    }
+    else if(feof(fp) && file_count >= file_num)
+    {
+        fr[i].flag = 1;
+        fclose(fp);
+        return -1;
+    }
+    fr[i].index = n * i;
+    ind = fr[i].index;
+
+    for(j=0; j<n; ++j)
+    {
+        if(fread(buff, sizeof(int), 1, fp) == 0)
+        {
+            fr[i].flag = 1;
+            ++files_read;
+            break;
+        }
+        // fread(buff, sizeof(int), 1, fp);
+        ip_buffer[ind] = buff[0];
+        ++ind;
+        fr[i].read_till += 1;
+    }
+    fclose(fp);
+    return 0;
+}
+
+void mergeGeneral(int runs, char *format, int size, char *op_file)
+{
+    // runs -> number of runs
+    // format -> file name format
+    // size -> part of each run
+    file_record fr[runs];
+    int ip_buffer[1000], op_buffer[1000];
+    int op_index, temp_index, min_value;
+    int i, written=0;
+    char file[30];
+    FILE *op;
     for(i=0; i<runs; ++i)
     {
-        sprintf(file, "%s.%03d", run_file, start+i);
+        sprintf(file, "%s.%03d", format, file_count);
+        ++file_count;
         fr[i].read_till = 0;
         fr[i].flag = 0;
         strcpy(fr[i].filename, file);
-        fillBuffer(i, size, ip_buffer, fr, num);
+        fillBuffer(i, size, ip_buffer, fr, format, runs);
     }
-    printf("size: %d\n", size);
-    printf("initial loaded\n");
-    written_runs = 0;    
     op_index = 0;
-    while(written_runs < runs)
+    written = 0;
+    files_read = file_count - runs;
+    // printf("files_read: %d\n", files_read);
+    // printf("files_count: %d\n", file_count);
+    while(written<runs || files_read<file_count)
     {
-        temp_index = -1;
         min_value = INT_MAX;
-
+        temp_index = -1;
+        // find the minimum
         for(i=0; i<runs; ++i)
         {
-            if(fr[i].flag==0 && min_value > ip_buffer[fr[i].index])
+            if(min_value > ip_buffer[fr[i].index])
             {
                 temp_index = i;
                 min_value = ip_buffer[fr[i].index];
             }
         }
-        if(min_value != INT_MAX)
+        if(temp_index != -1)
         {
             op_buffer[op_index++] = min_value;
             ip_buffer[fr[temp_index].index] = INT_MAX;
             fr[temp_index].index += 1;
+            if((fr[temp_index].index)%size == 0 && fr[temp_index].flag==0)
+            {
+                fillBuffer(temp_index, size, ip_buffer, fr, format, runs);
+            }
         }
-        if(op_index >= size*runs)
+
+        if(op_index >= 1000)
         {
-            op = fopen(filename, "ab+");
-            fwrite(op_buffer, sizeof(int), size*runs, op);
+            ++written;
+            // printf("in op_index: %d\n", op_index);
+            op = fopen(op_file, "ab+");
+            fwrite(op_buffer, sizeof(int), 1000, op);
             fclose(op);
-            ++written_runs;
             op_index = 0;
-        }
-        if(fr[temp_index].index%size == 0)
-        {
-            r = fillBuffer(temp_index, size, ip_buffer, fr, num);
-            if(r == -1)
-                break;
         }
     }
     if(op_index != 0)
     {
-        op = fopen(filename, "ab+");
-        fwrite(op_buffer, sizeof(int), (size_t)op_index, op);
+        op = fopen(op_file, "ab+");
+        fwrite(op_buffer, sizeof(int), op_index, op);
         fclose(op);
-    }
+    } 
+}
+
+void basicMergesort()
+{
+    // we will do 25-way merge
+    int size;
+    file_count = 0;
+    size = 1000 / total_runs;
+    mergeGeneral(total_runs, "input.bin", size, op_filename);
 }
 
 void multistepMerge(int n)
@@ -254,25 +199,24 @@ void multistepMerge(int n)
     temp = total_runs;
     // printf("total runs: %d\n", total_runs);
     // printf("super runs: %d\n", super_runs);
+    file_count = 0;
     for(i=0; i<super_runs; ++i)
     {
         sprintf(filename, "input.bin.super.%03d", i);
         printf("%s\n", filename);
         printf("temp: %d\n", temp);
         if(temp > n)
-            merge(n, i, n*i, "input.bin", filename);
+            mergeGeneral(n, "input.bin", 1000/n, filename);
         else
-            merge(temp, i, n*i, "input.bin",filename);
+            mergeGeneral(temp, "input.bin", 1000/temp, filename);
         temp -= n;
     }
-    merge(super_runs, 0, 0, "input.bin.super", op_filename);
+    file_count = 0;
+    mergeGeneral(super_runs, "input.bin.super", 1000/super_runs, op_filename);
 }
 
 int main(int argc, char *argv[])
 {
-    // scanf("%s", method);
-	// scanf("%s", ip_filename);
-	// scanf("%s", op_filename);
     strcpy(method, argv[1]);
     strcpy(ip_filename, argv[2]);
     strcpy(op_filename, argv[3]);
